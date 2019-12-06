07472cdbf785e3b0477c909434ea4c037ae454dc
JajunKoo <58553955+JajunKoo@users.noreply.github.com>
2019-12-07 00:36:05 +0900

    Update KoNLP.R

--- KoNLP.R
+++ KoNLP.R
@@ -5,7 +5,7 @@
 # Referenced from http://https://cran.r-project.org/web/packages/KoNLP/vignettes/KoNLP-API.html
 
 # load packages
-library(KoNLP)
+library(KoNLP)  # Korean NLP Package
 
 # Use dictionary
 useNIADic()     # use Insighter and Woorimalsam dictionary
@@ -13,11 +13,11 @@ useSejongDic()  # use Sejong noun dictionary
 useSystemDic()  # use system default dictionary
 
 # Get dictionary
-get_dictionary('user_dic')  # dic_name: woorimalsam, insighter, sejong, user_dic
+get_dictionary(dic_name = 'user_dic')  # dic_name: woorimalsam, insighter, sejong, user_dic
 
 # Build dictionary
-buildDictionary(ext_dic = c('sejong','woorimalsam','insighter'),
-                user_dic = data.frame(term="구자준", tag='ncn'),
+buildDictionary(ext_dic = c('sejong', 'woorimalsam', 'insighter'),
+                user_dic = data.frame(term="족발", tag='ncn'),
                 replace_usr_dic = T)
 
 statDic()  # Summary of dictionaries
